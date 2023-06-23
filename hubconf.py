c6a634b3c5290b35a8d8c95aed32275a6c8af4bd
Peter Melchior <peter.m.melchior@gmail.com>
2023-06-23 00:22:55 -0400

    added hub import

--- hubconf.py
+++ hubconf.py
@@ -1,11 +1,12 @@
-dependencies = ['equinox']
+dependencies = ['equinox', 'eqxhub']
 import equinox as eqx
+import eqxhub as hub
 from eqxhubtest import WhatEver as _WhatEver
 
 def test():
     model = _WhatEver()
     url = "https://hub.pmelchior.net/test.eqx"
-    file = hub.get_leave_file_from_url(url)
+    file = hub.get_leaves_file_from_url(url)
     filter_spec = lambda f,x: (
         str(f.read(), 'utf-8') if isinstance(x, str) else eqx.default_deserialise_filter_spec(f,x)
     )
